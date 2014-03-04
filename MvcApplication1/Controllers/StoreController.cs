1cf0d9f1ef3ff4b2efb415decf353216822cb1c5
Jordan Balintac <jordanbalintac08@yahoo.com>
2014-03-04 23:14:31 +0800

    Initial Add of Files

---
+++ MvcApplication1/Controllers/StoreController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Site.Models;              
+
+namespace Site.Controllers
+{
+    public class StoreController : Controller
+    {
+        MusicStoreEntities storeDB = new MusicStoreEntities();
+
+        //
+        // GET: /Store/
+        public ActionResult Index()
+        {
+            var genres = storeDB.Genres.ToList();
+
+            return View(genres);
+        }
+
+        //
+        // GET: /Store/Browse
+        public ActionResult Browse(string genre)
+        {
+            var genreModel = storeDB.Genres.Include("Albums").Single(a => a.Name == genre);
+
+            return View(genreModel);
+        }
+        //
+        // GET: /Store/Details
+        public ActionResult Details(int id)
+        {
+            var album = storeDB.Albums.Find(id);
+
+            return View(album);
+        }
+        
+
+    }
+}
